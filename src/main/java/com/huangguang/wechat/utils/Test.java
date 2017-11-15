dc8c5160433bbe91bba4d5d752c5f046856767a8
huangguang@anchol.com <huangguang@ancho.com>
2017-11-15 21:47:50 +0800

    commit

---
+++ src/main/java/com/huangguang/wechat/utils/Test.java
@@ -0,0 +1,11 @@
+package com.huangguang.wechat.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Administrator
+ * Date: 2017-10-14
+ * Time: 下午 11:53
+ */
+public class Test {
+}
---
+++ src/main/java/com/huangguang/wechat/utils/TestUtil.java
@@ -0,0 +1,9 @@
+package com.huangguang.wechat.utils;
+
+public class TestUtil {
+    private static final String s ="1";
+
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
